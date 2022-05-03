b11bee03cc8d458a856a57db6584fbe9c44d40ae
timo.schlumpf <70700633+Wadafacc@users.noreply.github.com>
2022-05-03 19:59:48 +0200

    ueue

--- components/Taskbar.vue
+++ components/Taskbar.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="tContainer">
+    <div class="header">
+      <div class="item">
+        <img src="~/static/icons/logo.png" alt="" />
+      </div>
+      <div class="item">
+        <h4>:D</h4>
+      </div>
+      <div class="item">
+        <h5>File</h5>
+      </div>
+      <div class="item">
+        <h5>Edit</h5>
+      </div>
+      <div class="item">
+        <h5>View</h5>
+      </div>
+    </div>
+
     <div class="taskbar">
       <img src="~/static/icons/37_finder.png" alt="" class="icon" />
       <img src="~/static/icons/07_terminal.png" alt="" class="icon" />
